7a641ed8595ff4ce96e732cf3db9a419fd1892d0
Pan <hypersausage@web.de>
2019-12-07 19:23:08 +0100

    Update chatbot_screen.dart

--- lib/screens/chatbot_screen.dart
+++ lib/screens/chatbot_screen.dart
@@ -10,6 +10,29 @@ class ChatbotScreen extends StatefulWidget {
 class ChatbotScreenState extends State<ChatbotScreen> {
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: "Enter Text",
+          fillColor: Colors.white,
+          border: new OutlineInputBorder(
+            borderRadius: new BorderRadius.circular(25.0),
+            borderSide: BorderSide(),
+          ),
+        ),
+        validator: (val) {
+          if (val.length == 0) {
+            return "Email cannot be empty";
+          } else {
+            return null;
+          }
+        },
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          fontFamily: "Poppins",
+        ),
+      ),
+    );
   }
 }

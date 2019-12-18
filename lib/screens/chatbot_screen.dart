8efd47038f1c1c14f422cd71a62ded7e812fb858
Pan <hypersausage@web.de>
2019-12-18 17:46:30 +0100

    Finishing Initila Chatbot Screen
    Need to change ChatMessage Class to get my desired look

--- lib/screens/chatbot_screen.dart
+++ lib/screens/chatbot_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentor_chatbot/widgets/chat_message.dart';
 
 class ChatbotScreen extends StatefulWidget {
   @override
@@ -7,32 +8,80 @@ class ChatbotScreen extends StatefulWidget {
   }
 }
 
-class ChatbotScreenState extends State<ChatbotScreen> {
+class ChatbotScreenState extends State<ChatbotScreen>
+    with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-      child: TextFormField(
-        decoration: InputDecoration(
-          labelText: "Enter Text",
-          fillColor: Colors.white,
-          border: new OutlineInputBorder(
-            borderRadius: new BorderRadius.circular(25.0),
-            borderSide: BorderSide(),
+    return new Column(
+      children: <Widget>[
+        new Flexible(
+          child: new ListView.builder(
+            padding: new EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
           ),
         ),
-        validator: (val) {
-          if (val.length == 0) {
-            return "Email cannot be empty";
-          } else {
-            return null;
-          }
-        },
-        keyboardType: TextInputType.text,
-        style: TextStyle(
-          fontFamily: "Poppins",
+        new Container(
+          child: _buildTextComposer(),
+          margin: EdgeInsets.all(5.0),
+          decoration: new BoxDecoration(
+              color: Theme.of(context).focusColor,
+              borderRadius: BorderRadius.circular(25.0)),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 200),
+        vsync: this,
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
   }
 }

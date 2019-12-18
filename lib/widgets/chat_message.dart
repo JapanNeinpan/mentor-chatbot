503d31e9ebc3a275f22e912e58e31cfbb5e4a5ac
Pan <hypersausage@web.de>
2019-12-18 21:14:26 +0100

    Finished Chatscreen bubbles

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,38 +1,33 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.animationController});
+  ChatMessage({this.text, this.animationController, this.isSent});
   final String text;
+  final bool isSent;
   final AnimationController animationController;
   @override
   Widget build(BuildContext context) {
-    return new SizeTransition(
-        sizeFactor: new CurvedAnimation(
-            parent: animationController, curve: Curves.easeIn),
-        axisAlignment: 0.0,
-        child: new Container(
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                margin: const EdgeInsets.only(right: 16.0),
-                child: new CircleAvatar(child: new Text("P")),
-              ),
-              new Expanded(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text("Pan", style: Theme.of(context).textTheme.subhead),
-                    new Container(
-                      margin: const EdgeInsets.only(top: 5.0),
-                      child: new Text(text),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
+    return Bubble(
+      child: Text(text),
+      nip: isSent ? BubbleNip.rightTop : BubbleNip.leftTop,
+      margin: isSent
+          ? BubbleEdges.only(left: 50.0, bottom: 5.0, top: 5.0)
+          : BubbleEdges.only(right: 50.0, bottom: 5.0, top: 5.0),
+      alignment: isSent ? Alignment.topRight : Alignment.topLeft,
+      color: isSent ? Color.fromRGBO(225, 255, 199, 1.0) : Colors.white,
+    );
+//    return new Container(
+//        margin: isSent
+//            ? EdgeInsets.only(left: 50.0, bottom: 5.0, top: 5.0)
+//            : EdgeInsets.only(right: 50.0, bottom: 5.0, top: 5.0),
+//        decoration: new BoxDecoration(
+//          borderRadius: BorderRadius.circular(5),
+//          color: isSent ? Colors.green : Colors.blueAccent,
+//        ),
+//        child: new Container(
+//          margin: const EdgeInsets.all(5.0),
+//          child: new Text(text),
+//        ));
   }
 }

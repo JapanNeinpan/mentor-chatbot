5de6271a5d314dc4d8b74040a1e0f3f9d427b94d
Pan <hypersausage@web.de>
2019-12-19 11:29:21 +0100

    Deleted comments

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -17,17 +17,5 @@ class ChatMessage extends StatelessWidget {
       alignment: isSent ? Alignment.topRight : Alignment.topLeft,
       color: isSent ? Color.fromRGBO(225, 255, 199, 1.0) : Colors.white,
     );
-//    return new Container(
-//        margin: isSent
-//            ? EdgeInsets.only(left: 50.0, bottom: 5.0, top: 5.0)
-//            : EdgeInsets.only(right: 50.0, bottom: 5.0, top: 5.0),
-//        decoration: new BoxDecoration(
-//          borderRadius: BorderRadius.circular(5),
-//          color: isSent ? Colors.green : Colors.blueAccent,
-//        ),
-//        child: new Container(
-//          margin: const EdgeInsets.all(5.0),
-//          child: new Text(text),
-//        ));
   }
 }
